4d6170366c913363e2f981b2f0569d92ec00f967
JayanthNaiduKundrapu <jayanth.kundrapu013@gmail.com>
2026-05-18 16:30:39 +0530

    exercise 4 : added additional tests

--- tests/test_students.py
+++ tests/test_students.py
@@ -16,3 +16,13 @@ def test_create_student(client):
     )
 
     assert response.status_code == 201
+
+def test_get_students(client):
+
+    response = client.get("/api/v1/students")
+    assert response.status_code == 200
+    assert isinstance(response.json, list)
+    assert len(response.json) >= 1
+    assert "name" in response.json[0]
+    assert "email" in response.json[0]
+    assert "age" in response.json[0]
