adf3125a05e5bc06a440dfd86b5896e76bf3b6ec
JayanthNaiduKundrapu <jayanth.kundrapu013@gmail.com>
2026-06-03 13:15:03 +0530

    refactored routes & tests

--- tests/test_students.py
+++ tests/test_students.py
@@ -21,10 +21,16 @@ def test_create_student(client):
 def test_get_students(client):
 
     response = client.get("/api/v1/students")
-    response = client.delete("/api/v1/students")
     assert response.status_code == 200
     assert isinstance(response.json, list)
     assert len(response.json) >= 1
     assert "name" in response.json[0]
     assert "email" in response.json[0]
     assert "age" in response.json[0]
+
+
+def test_delete_student(client):
+
+    response = client.delete("/api/v1/students")
+    assert response.status_code == 405
+
